45ccf052ddcefef4d0b443e95f4e40ea4dcefbd2
thawbaan <thawbaan@thawbaans-MacBook-Air.local>
2024-01-31 10:53:17 +0530

    first commit

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+
+  appointment: string = "Take Dog for a walk";
+}
